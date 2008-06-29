a3a2638da2c66bd65044a372b9b5517c44744945
mpvenable <mpvenable@sourceforge.net>
2008-06-29 07:19:00 +0000

    Implemented the two delete functions.  Cleaned up the code.  Think it is done now.

--- src/Reverter/WindowsRegistry/WindowsRegistry.py
+++ src/Reverter/WindowsRegistry/WindowsRegistry.py
@@ -1,77 +1,122 @@
 """This file contains classes for working with the Windows
 registry.
 
-Author: Michael Venable"""
+Author: Michael Venable
+"""
 
-from _winreg import OpenKey, QueryValueEx
+import _winreg
+
 
 #Define exceptions
 class WindowsRegistryException(Exception):
     """Represents an error that has occurred while interacting
-    with the Windows registry."""
-    def __init__(self, message, source):
-        """Initializes this exception.  Includes a message describing
-        why the error occurred and a source exception."""
+    with the Windows registry.
+    """
+    def __init__(self, message, source=None):
+        """Initializes this exception.
+
+        message -   Description of the error.
+        source -    Original exception that caused this error.  None
+                    if there is no originating exception.
+        """
         Exception.__init__(self, message)
         self.source = source
-class CantOpenKeyException(WindowsRegistryException):
-    """Indicates a registry key could not be accessed."""
-    def __init__(self, message, source):
-        WindowsRegistryException.__init__(self, message, source)
-class CantRemoveKeyException(WindowsRegistryException):
-    """INDICATEs a registry key could not be deleted."""
-    def __init__(self, message, source):
-        WindowsRegistryException.__init__(self, message, source)
 
+#Define classes
 class WindowsRegistry:
     """Abstraction of the Windows registry."""
 
     def getValue(self, key):
         """Retrieves the value data associated with the given
-        key and value name.  Throws a CantOpenKeyException if
-        the key does not exist or could not be accessed."""
+        key and value name. 
+
+        key -   The key/value to retrieve.  Example:
+                RegistryKey("HKEY_CURRENT_USER\\Console\\CursorSize")
+                
+        Throws WindowsRegistryException if the value could not be
+        accessed.
+        """
+        keyHandle = None
         try:
-            keyHandle = OpenKey( key.hive(), key.subKey() );
-            value = QueryValueEx(keyHandle, key.valueName())
-            keyHandle.Close()
+            keyHandle = _winreg.OpenKey( key.hive(), key.subKey() )
+            value = _winreg.QueryValueEx(keyHandle, key.valueName())
             return value[0]
-        except _winreg.EnvironmentError, e:
-            raise CantOpenKeyException("Unable to open the key " + key, e)
+        except Exception, e:
+            raise WindowsRegistryException("Unable to open the key " + str(key), e)
+        finally:
+            if keyHandle is not None:
+                keyHandle.Close()
         
     def removeKey(self, key):
         """Permanently removes the specified key from the
-        registry.  Throws an exception is the key does not
-        exist or could not be removed."""
-        raise Exception("This method is not implemented.")
+        registry. 
+
+        key - Key to be removed. Example:
+              RegistryKey("HKEY_CURRENT_USER\\Control Panel")
+
+        Throws WindowsRegistryException if the key cannot be
+        deleted.
+        """
+        keyHandle = None
+        try:
+            keyHandle = _winreg.OpenKey( key.hive(), key.subKey(), 0, _winreg.KEY_ALL_ACCESS )
+            _winreg.DeleteKey(keyHandle, key.valueName())
+        except Exception, e:
+            raise WindowsRegistryException("Unable to open the key " + str(key), e)
+        finally:
+            if keyHandle is not None:
+                keyHandle.Close()
 
     def removeValue(self, key):
-        """Permanently removed the specified value from teh
-        registry."""
-        raise Exception("This method is not implemented.")
+        """Permanently removes the specified value from teh
+        registry.  
+
+        key -   The key/value to be removed.  Example:
+                RegistryKey("HKEY_CURRENT_USER\\Control Panel\\buttoncolor")
+
+        Throws WindowsRegistryException if the value cannot be
+        removed.
+        """
+        keyHandle = None
+        try:
+            keyHandle = _winreg.OpenKey( key.hive(), key.subKey(), 0, _winreg.KEY_ALL_ACCESS )
+            _winreg.DeleteValue(keyHandle, key.valueName())
+        except Exception, e:
+            raise WindowsRegistryException("Unable to open the key " + str(key), e)
+        finally:
+            if keyHandle is not None:
+                keyHandle.Close()
 
 class RegistryKey:
     """Represents an entire key and value name in the Windows
-    registry.  This class represents the key, subkey, and value
-    name.  A sample key is 'HKEY_CURRENT_USER.'  A sample
-    subkey is 'Control Panel\Accessibility.'  A sample value name
-    is 'MinimumHitRadius.'"""
+    registry, or represents only a key without the value name.
+    """
 
     SEPARATOR = '\\'
     
     def __init__(self, key):
-        """Creates this.  key is the full key, including hive, subkey,
-        and value name. A sample key is 'HKEY_CURRENT_USER\Console\CursorSize'."""
+        """Creates this.
+
+        key - the full key name, including hive, subkey, and
+              optional value name.  Examples:
+              "HKEY_CURRENT_USER\\Console\\CursorSize" if
+              value name is included, or 
+              "HKEY_CURRENT_USER\\Console" if value name is
+              not included.
+        """
         self.keys = key.split(self.SEPARATOR)
 
     def __getitem__(self, key):
         """Accesses a piece of this key. For example, if the key is
-        'HKEY_CURRENT_USER\Console\CurrentUser', then key[1] is
-        'Console'."""
+        'HKEY_CURRENT_USER\Console\CurrentUser', then key[1] will
+        return 'Console'.
+        """
         return self.keys[key]
 
     def __len__(self):
         """Returns the number of parts that make up this key.
-        For example, for the key "ROOT\aaa\bbb\ccc", len(self) is four."""
+        For example, for the key "ROOT\aaa\bbb\ccc", len(self) is four.
+        """
         return len(self.keys)
 
     def __setitem__(self, key, item):
@@ -79,28 +124,37 @@ class RegistryKey:
         self.keys[key] = item
 
     def __str__(self):
+        """String representation of this key."""
         return self.makeFullKey()
 
     def hive(self):
         """Returns the hive of this key.  The hive is a constant
         defined in _winreg and can be one of _winreg.HKEY_CLASSES_ROOT,
         _winreg.HKEY_CURRENT_USER, _winreg.HKEY_LOCAL_MACHINE,
-        _winreg.HKEY_USERS, _winreg.HKEY_CURRENT_CONFIG."""
+        _winreg.HKEY_USERS, _winreg.HKEY_CURRENT_CONFIG.
+
+        Throws WindowsRegistryException if the hive name is not a
+        valid Windows hive name.
+        """
         if self.hiveName() == "HKEY_CLASSES_ROOT":
-            return wreg.HKEY_CLASSES_ROOT
+            return _winreg.HKEY_CLASSES_ROOT
         elif self.hiveName() == "HKEY_CURRENT_USER":
-            return wreg.HKEY_CURRENT_USER
+            return _winreg.HKEY_CURRENT_USER
         elif self.hiveName() == "HKEY_LOCAL_MACHINE":
-            return wreg.HKEY_LOCAL_MACHINE
+            return _winreg.HKEY_LOCAL_MACHINE
         elif self.hiveName() == "HKEY_USERS":
-            return wreg.HKEY_USERS
+            return _winreg.HKEY_USERS
         elif self.hiveName() == "HKEY_CURRENT_CONFIG":
-            return wreg.HKEY_CURRENT_CONFIG
+            return _winreg.HKEY_CURRENT_CONFIG
+        else:
+            raise WindowsRegistryException(self.hiveName() + \
+                " is not a Windows registry hive name.")
         
     def hiveName(self):
         """Returns the name of the top-level key or hive.
         For example, if this key is 'HKEY_CURRENT_USER\Console\CursorSize',
-        then self.hiveName() is 'HKEY_CURRENT_USER'."""
+        then self.hiveName() is 'HKEY_CURRENT_USER'.
+        """
         return self[0]
     
     def makeFullKey(self):
@@ -109,12 +163,16 @@ class RegistryKey:
 
     def subKey(self):
         """Returns this key's subkey.  For example, if this key is
-        'HKEY_CURRENT_USER\Console\Cursors\CursorSize', then self.subKey() is
-        'Console\Cursors'."""
+        "HKEY_CURRENT_USER\Console\Cursors\CursorSize," then self.subKey() is
+        'Console\Cursors'.  If the key contains only two components, as in
+        "HKEY_CURRENT_USER\Console," then there is not subKey and this
+        method returns an empty string.
+        """
         return self.SEPARATOR.join( self[1: len(self)-1] )
     
     def valueName(self):
         """Returns this key's value name.  For example, if this key is
-        'HKEY_CURRENT_USER\Console\CursorSize', then self.valueName() is
-        'CursorSize'."""
+        "HKEY_CURRENT_USER\Console\CursorSize", then self.valueName() is
+        "CursorSize."
+        """
         return self[ len(self)-1 ]
