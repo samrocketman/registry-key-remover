33fc740a1a290a01239ac7d495d4881420ea403c
coreyfournier <coreyfournier@sourceforge.net>
2008-06-30 01:26:22 +0000

    Added regular expressions that allow easy access to the values added and keys added sections.

--- src/Reverter/EntryList/RegShotListReader.py
+++ src/Reverter/EntryList/RegShotListReader.py
@@ -1,11 +1,22 @@
 import os
+import re
 
 class RegShotListReader:
+    VALUES_ADDED_EXPRESSION = "Values added"
+    KEYS_ADDED_EXPRESSION = "Keys added"
+    REGULAR_EXPRESSION =  "\:[0-9]{0,3}(.)+\n(.)+(.|\s|\n)*?[-]{33}"
     def __init__(self, fileName):
         self.fileName = fileName
+        f = open(self.fileName,'r')
+        p = re.compile(VALUES_ADDED_EXPRESSION + REGULAR_EXPRESSION)
+        m = p.search(f.read())
+        if m:
+            print 'Match found: ', m.group()
+        else:
+            print 'No match'
 
     def __getitem__(self,key):
-        f=open(self.fileName,'r')
+        
         return f
         
     def getItem(self):
