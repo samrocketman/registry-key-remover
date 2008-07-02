af0ee624604828dcacc520ecba148622654eb298
coreyfournier <coreyfournier@sourceforge.net>
2008-07-02 02:20:38 +0000

    Added code to remove the registry entries. I have not tested anything since it will not run from my Linux box. The file name is still hard coded.

--- src/Reverter/__init__.py
+++ src/Reverter/__init__.py
@@ -1,16 +1,31 @@
-#Main driver for application
-#@author: Corey Fournier
-
+"""
+    Main driver for application
+    Author: Corey Fournier
+"""
 from EntryList.RegShotListReader import RegShotListReader
+from WindowsRegistry.WindowsRegistry import WindowsRegistry
+
+TAB = "\t"
+NEW_LINE = "\n"
 
 fileName = "/home/corey/workspace/HelloWorld/InstallRemove.txt"
 
-el = RegShotListReader(fileName)
+registryInterface = WindowsRegistry()
+registryList = RegShotListReader(fileName)
 
-#el.getItem()
-for line in el.getKeys():
-    print line
+print "Processing all keys\n"
+for line in registryList.getKeys():
+    print TAB + "Removing: " + line
+    registryInterface.removeKey(line.strip())
 
-print "=============================================================="
-for line in el.getValues():
+print NEW_LINE
+print "Processing all values\n"
+
+for line in registryList.getValues():
+    print TAB + "Removing: " + line
+    registryInterface.removeValue(line.strip())
     print line
+    
+print NEW_LINE    
+print "All done, Thanks for using Corey And Mike's Registry Reverter"
+print NEW_LINE
