4855064f53b907e6ba02e5fdcc008eea7ded442b
coreyfournier <coreyfournier@sourceforge.net>
2008-07-03 01:01:41 +0000

    Added code to read in the file name. Still no error checking.

--- src/Reverter/__init__.py
+++ src/Reverter/__init__.py
@@ -4,11 +4,20 @@
 """
 from EntryList.RegShotListReader import RegShotListReader
 from WindowsRegistry.WindowsRegistry import WindowsRegistry
+from WindowsRegistry.WindowsRegistry import RegistryKey
+import sys
 
 TAB = "\t"
 NEW_LINE = "\n"
+FILE_NAME_ARGUMENT_POSITION = 1
 
-fileName = "/home/corey/workspace/HelloWorld/InstallRemove.txt"
+"""Check for the file name passed in"""
+if sys.argv[FILE_NAME_ARGUMENT_POSITION] == "":
+    print "File name as an argument is required" + NEW_LINE
+    exit()
+else :
+    fileName = sys.argv[FILE_NAME_ARGUMENT_POSITION] 
+    print "Using file '" + fileName + "'" + NEW_LINE
 
 registryInterface = WindowsRegistry()
 registryList = RegShotListReader(fileName)
@@ -16,15 +25,16 @@ registryList = RegShotListReader(fileName)
 print "Processing all keys\n"
 for line in registryList.getKeys():
     print TAB + "Removing: " + line
-    registryInterface.removeKey(line.strip())
+    keyInstance = RegistryKey(line.strip())
+    registryInterface.removeKey(keyInstance)
 
 print NEW_LINE
 print "Processing all values\n"
 
 for line in registryList.getValues():
     print TAB + "Removing: " + line
-    registryInterface.removeValue(line.strip())
-    print line
+    keyInstance = RegistryKey(line.strip())
+    registryInterface.removeValue(keyInstance)
     
 print NEW_LINE    
 print "All done, Thanks for using Corey And Mike's Registry Reverter"
