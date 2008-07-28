c8c52159527feed35defe899df23535e07d276a7
coreyfournier <coreyfournier@sourceforge.net>
2008-07-28 02:00:39 +0000

    added a reference to a new class that parses the switches in the command line.
    No testing has been done with all of the new code in place.

--- src/Reverter/__init__.py
+++ src/Reverter/__init__.py
@@ -6,22 +6,30 @@ from EntryList.RegShotListReader import RegShotListReader
 from WindowsRegistry.WindowsRegistry import WindowsRegistry
 from WindowsRegistry.WindowsRegistry import RegistryKey
 from WindowsRegistry.WindowsRegistry import WindowsRegistryException
+from SwitchParser import *
 import sys
+
 
 TAB = "\t"
 NEW_LINE = "\n"
 FILE_NAME_ARGUMENT_POSITION = 1
 
-"""Check for the file name passed in"""
-if sys.argv[FILE_NAME_ARGUMENT_POSITION] == "":
-    print "File name as an argument is required" + NEW_LINE
-    exit()
-else :
-    fileName = sys.argv[FILE_NAME_ARGUMENT_POSITION] 
-    print "Using file '" + fileName + "'" + NEW_LINE
+sp = SwitchParser(sys.argv)
 
 registryInterface = WindowsRegistry()
-registryList = RegShotListReader(fileName)
+registryList = RegShotListReader(sp.fileName)
+
+if sp.deleteWithCascade :
+    print "Deleting all keys with cascade\n"
+    for line in registryList.getKeys():
+        print TAB + "Removing: " + line
+        keyInstance = RegistryKey(line.strip())
+        try:
+            registryInterface.removeKeyCascade(keyInstance)
+            pass        
+        except WindowsRegistryException, e:
+            print e
+
 
 print "Processing all keys\n"
 for line in registryList.getKeys():
@@ -44,7 +52,9 @@ for line in registryList.getValues():
         #pass
     except WindowsRegistryException, e:
         print e
-    
+   
 print NEW_LINE    
 print "All done, Thanks for using Corey And Mike's Registry Reverter"
 print NEW_LINE
+
+
