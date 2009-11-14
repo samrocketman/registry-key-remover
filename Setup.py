74b7f13cb960ca0c9b1f46fab0f82347d9e92bc2
Sam Gleske <sam.mxracer@gmail.com>
2009-11-14 22:58:48 +0000

    Added more setup information.

--- Setup.py
+++ Setup.py
@@ -1,4 +1,11 @@
 from distutils.core import setup
 import py2exe
-
-setup(console=['src\Reverter\__init__.py'])
+# Filled out required information for setup.py from http://docs.python.org/distutils/setupscript.html
+setup(name="Registry Key Remover",
+      #Version = Major.Minor.PatchSet
+      version="0.1.42",
+      description="Removes registry keys based on the snap shot provided by RegShot.",
+      url="https://sourceforge.net/projects/registrykeyremo/",
+      author="coreyfournier, mpvenable",
+      author_email="@users.sourceforge.net",
+      console=['src/Reverter/__init__.py'])
